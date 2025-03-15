d86410af27f65c462c11651775e2cd10cb89dbc1
Reuben <dsareuben.2003@gmail.com>
2025-03-15 16:32:37 +0530

    commit new

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const carouselContainer = document.querySelector('.carousel-container');
 const images = document.querySelectorAll('.carousel img');
 const prevBtn = document.querySelector('.prev');
@@ -27,33 +26,3 @@ setInterval(() => {
     index = (index + 1) % images.length;
     showSlide();
 }, 5000);
-=======
-const carouselContainer = document.querySelector('.carousel-container');
-const images = document.querySelectorAll('.carousel img');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-
-let index = 0;
-
-function showSlide() {
-    carouselContainer.style.transform = `translateX(${-index * 100}%)`;
-}
-
-// Next Button
-nextBtn.addEventListener('click', () => {
-    index = (index + 1) % images.length; // Loops back to the first image
-    showSlide();
-});
-
-// Previous Button
-prevBtn.addEventListener('click', () => {
-    index = (index - 1 + images.length) % images.length; // Loops to the last image
-    showSlide();
-});
-
-// Auto-slide every 3 seconds
-setInterval(() => {
-    index = (index + 1) % images.length;
-    showSlide();
-}, 5000);
->>>>>>> refs/remotes/origin/main
